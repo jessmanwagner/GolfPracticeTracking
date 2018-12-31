041bcac35506d14e5874d88e821960c987af92e6
Jessman Wagner <jwagner@open-techs.com>
2018-12-31 11:28:21 -0700

    initial work on csv file upload

---
+++ GolfPracticeTracker/Models/SkyTrakCsvFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GolfPracticeTracker.Models
+{
+    public class SkyTrakCsvFile
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        public string PrivateSchedule { get; set; }
+
+        [Display(Name = "Private Schedule Size (bytes)")]
+        [DisplayFormat(DataFormatString = "{0:N1}")]
+        public long PrivateScheduleSize { get; set; }
+
+        [Display(Name = "Uploaded (UTC)")]
+        [DisplayFormat(DataFormatString = "{0:F}")]
+        public DateTime UploadDT { get; set; }
+    }
+}
