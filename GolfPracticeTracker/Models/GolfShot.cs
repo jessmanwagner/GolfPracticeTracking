ee15acdb5b8f96ac8930b51a7b8a0d88bdd54a4c
Jessman Wagner <jwagner@open-techs.com>
2019-01-18 16:47:14 -0700

    minor changes

--- GolfPracticeTracker/Models/GolfShot.cs
+++ GolfPracticeTracker/Models/GolfShot.cs
@@ -8,20 +8,20 @@ namespace GolfPracticeTracker.Models
         public int ID { get; set; }
         public int PracticeSessionID { get; set; }
         public int ShotNumber { get; set; }
-        public double BallMph { get; set; }
-        public double ClubMph { get; set; }
-        public double LaunchDeg { get; set; }
+        public double BallMph { get; set; }             //both
+        public double ClubMph { get; set; }             //both
+        public double LaunchDeg { get; set; }           //both
         public double SideDeg { get; set; }
-        public int BackSpinRpm { get; set; }
-        public int SideSpinRpm { get; set; }
+        public int BackSpinRpm { get; set; }            //both
+        public int SideSpinRpm { get; set; }            //both
         public double FlightSeconds { get; set; }
-        public int DescentDeg { get; set; }
-        public double HeightYards { get; set; }
+        public int DescentDeg { get; set; }             //both
+        public double HeightYards { get; set; }         //both
         public double PtiScore { get; set; }
-        public int OfflineYards { get; set; }
-        public int CarryYards { get; set; }
+        public int OfflineYards { get; set; }           //both
+        public int CarryYards { get; set; }             //both
         public int RollYards { get; set; }
-        public int TotalYards { get; set; }
+        public int TotalYards { get; set; }             //both
 
         //Navigation Properties
         public PracticeSession PracticeSession { get; set; }
